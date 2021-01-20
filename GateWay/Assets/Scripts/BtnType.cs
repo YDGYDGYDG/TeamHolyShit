2603729cd91460bc7adb1816a408d847edcec596
YooKeonHee <rjsgml4353@naver.com>
2021-01-20 22:50:24 +0900

    메인로비 추가
    메인로비 추가

---
+++ GateWay/Assets/Scripts/BtnType.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public BTNType currentType;
+    public Transform buttonScale;
+    Vector3 defaultScale;
+    public CanvasGroup mainGroup;
+    public CanvasGroup optionGroup;
+
+    bool isSound;
+    public void Start()
+    {
+        defaultScale = buttonScale.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.New:
+                SceneManager.LoadScene("Game");
+                break;
+            case BTNType.Continue:
+                SceneManager.LoadScene("Game");
+                break;
+            case BTNType.Option:
+                CanvasGroupOn(optionGroup);
+                CanvasGroupOff(mainGroup);
+                break;
+            case BTNType.Sound:
+                if (isSound)
+                {
+                    Debug.Log("사운드OFF");
+                }
+                else
+                {
+                    Debug.Log("사운드ON");
+                }
+                isSound = !isSound;
+                break;
+            case BTNType.Back:
+                CanvasGroupOn(mainGroup);
+                CanvasGroupOff(optionGroup);
+                break;
+            case BTNType.Quit:
+                Application.Quit();
+                Debug.Log("앱종료");
+                break;
+        }
+
+    }
+    public void CanvasGroupOn(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+}
---
+++ GateWay/Assets/Scripts/MainUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum BTNType
+{
+    New,
+    Continue,
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+
+public class MainUI : MonoBehaviour
+{
+
+}

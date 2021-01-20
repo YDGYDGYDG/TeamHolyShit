4a554075fe82bd23f96ac31412c4076e7611c03f
Nayeon- <rlaskdus0102@gmail.com>
2021-01-20 18:45:10 +0900

    사라지는 벽 스크립트
    사라집니다 벽이

---
+++ GateWay/Assets/Scripts/Disappear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+
+    private CapsuleCollider2D capsule; // 충돌 처리해주기 위한 컴포넌트
+   
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "diswall")
+        {
+            Debug.Log("제발실행해젭ㄷㄱㄹ ㅓㅐㅇ");
+            Destroy(collision.gameObject, 1);
+            GameObject.Find("player").GetComponent<HookShotScript>().HookOFF();
+        }
+    }
+}

9f0181d655dbbb0e16ccd4a9c870ba47acebc35a
YooKeonHee <77230130+song9318@users.noreply.github.com>
2021-01-22 17:53:15 +0900

    옵션>사운드 볼륨조절기능추가
    옵션>사운드 볼륨조절기능추가

---
+++ GateWay/Assets/Scripts/OptionSoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionSoundManager : MonoBehaviour
+{
+    public AudioSource musicsource;
+    public AudioSource btnsource;
+    
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+    }
+
+    public void SetButtonVolume(float volume)
+    {
+        btnsource.volume = volume;
+    }
+
+    public void OnSfx()
+    {
+        btnsource.Play();
+    }
+
+
+}

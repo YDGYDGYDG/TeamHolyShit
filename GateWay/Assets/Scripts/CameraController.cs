200912992a6d123767e1934df6dfdbdd5db0828c
YDGYDGYDG <tlsaoss@naver.com>
2021-01-22 11:23:51 +0900

    카메라 컨트롤러
    카메라가 기본적으로는 캐릭터를 중심에 두는 형태로 따라가지만, 범위를 나가면 안따라감

---
+++ GateWay/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float heightMax = 1000;
+    public float heightMin = -1000;
+    public float widthMax = 1000;
+    public float widthMin = -1000;
+
+    public GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.Find("player");
+    }
+
+    void Update()
+    {
+        if (transform.position.x < widthMax && transform.position.x > widthMin && transform.position.y < heightMax && transform.position.y > heightMin)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}

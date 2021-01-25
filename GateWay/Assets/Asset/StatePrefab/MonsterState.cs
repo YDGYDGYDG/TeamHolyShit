089a35fdc368aac30006016cc5fa854bee3b4b32
hjuni0524 <hjuni0524@naver.com>
2021-01-26 00:58:56 +0900

    하단 세부 내용 참조
    1. 'Coin' 프리팹 세팅 및 생성
    2. 용암 테마 맵 기믹 스크립트 생성중
    3. 'PlayerState' 코드 수정
    4. 그 외 기타 작업 진행중....

--- GateWay/Assets/Asset/StatePrefab/MonsterState.cs
+++ GateWay/Assets/Asset/StatePrefab/MonsterState.cs
@@ -5,30 +5,34 @@ using UnityEngine.UI;
 
 public class MonsterState : MonoBehaviour
 {
-    
+
+    HookShotScript hookLine;    // 훅 연결용 변수
 
     public GameObject monsterDeath;
-
-    
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if(col.gameObject.tag == "Hook")        // 너 밧줄이랑 충돌했니??
-        {
-            this.gameObject.SetActive(false);   // 그럼 뒤지삼
-            Instantiate(monsterDeath, transform.position, Quaternion.identity);  // 이펙트도 출력해
-            
-            
-        }
-    }
-
 
     // Start is called before the first frame update
     void Start()
     {
 
-
+        hookLine = GameObject.Find("player").GetComponent<HookShotScript>();    // 스크립트 연결
 
     }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Hook")        // 너 밧줄이랑 충돌했니??
+        {
+
+            this.gameObject.SetActive(false);   // 그럼 뒤지삼
+            Instantiate(monsterDeath, transform.position, Quaternion.identity);  // 이펙트도 출력해
+            hookLine.HookOFF();                   // 훅도 지워줘야지??
+
+        }
+    }
+
+
+    
 
     
 
--- GateWay/Assets/Asset/StatePrefab/PlayerState.cs
+++ GateWay/Assets/Asset/StatePrefab/PlayerState.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerState : MonoBehaviour
 {
 
     HookShotScript hookLine;    // 훅 연결용 변수
-    public GameObject playerDeath;
+
+    public GameObject playerDeath;  // 플레이어 죽었을 때
+
+    public GameObject takeCoin;     // 코인 획득했을 때
+    GameObject Coin;                // 코인 연결용
 
     // Start is called before the first frame update
     void Start()
     {
         hookLine = GameObject.Find("player").GetComponent<HookShotScript>();    // 훅샷 스크립트 연결
+        Coin = GameObject.Find("Coin");     // 코인 연결
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -21,18 +27,29 @@ public class PlayerState : MonoBehaviour
             this.gameObject.SetActive(false);   // 그럼 뒤지삼
             hookLine.HookOFF();                 // 훅도 지워줘야지??
             Instantiate(playerDeath, transform.position, Quaternion.identity);    // 이펙트도 출력해
+        }
 
-            
+        else if (col.gameObject.tag == "Trap")        // 너 함정이랑 충돌했니??
+        {
+            this.gameObject.SetActive(false);   // 그럼 뒤지삼
+            hookLine.HookOFF();                 // 훅도 지워줘야지??
+            Instantiate(playerDeath, transform.position, Quaternion.identity);    // 이펙트도 출력해
+        }
+
+        else if (col.gameObject.tag == "Star")        // 너 동전이랑 충돌했니??
+        {
+            Coin.SetActive(false);                 // 동전 지워줘
+            Instantiate(takeCoin, transform.position, Quaternion.identity);    // 이펙트도 출력해
         }
     }
 
 
-
-    
-
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+
 }

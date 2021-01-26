5007b3ea068b0738579dc37c15a5f7632f0f2ba0
Nayeon- <rlaskdus0102@gmail.com>
2021-01-26 15:18:53 +0900

    물업데이트

--- GateWay/Assets/Scripts/StarController.cs
+++ GateWay/Assets/Scripts/StarController.cs
@@ -10,7 +10,7 @@ public class StarController : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {

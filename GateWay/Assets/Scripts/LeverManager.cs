22a2a39e6bfa8275540b4d827dd4403a3d9ad187
YooKeonHee <rjsgml4353@naver.com>
2021-01-26 14:57:07 +0900

    일정시간 뒤 사라지는발판 구현
    일정시간 뒤 사라지는발판 구현

---
+++ GateWay/Assets/Scripts/LeverManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject Lever;
+    public GameObject wall;
+    float disappearTime = 3.0f;  // 3초뒤 레버가 다시 돌아옴   
+
+    void Appear()
+    {
+        Lever.GetComponent<SpriteRenderer>().material.color = Color.red;
+        wall.GetComponent<SpriteRenderer>().material.color = Color.white;
+    }
+    void DisAppear()
+    {
+        Lever.GetComponent<SpriteRenderer>().material.color = Color.blue;
+        wall.GetComponent<SpriteRenderer>().material.color = new Color(255, 255, 255, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Lever")
+        {
+            Appear();
+            Invoke("DisAppear", disappearTime);
+        }
+    }
+}

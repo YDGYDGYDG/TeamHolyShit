d24a870889ce4389ff6a3ff4d8cf48c555743443
YDGYDGYDG <tlsaoss@naver.com>
2021-01-27 10:41:39 +0900

    포탈
    포탈

---
+++ GateWay/Assets/Scripts/Object/MachineryPlanet/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject portalOut;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = portalOut.transform.position + Vector3.up;
+        }
+    }
+}

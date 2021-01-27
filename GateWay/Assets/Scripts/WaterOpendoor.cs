b374fd6f92fc24441eecf5e2b952734334dec29f
Nayeon- <rlaskdus0102@gmail.com>
2021-01-27 16:43:41 +0900

    물 행성 업뎃

--- GateWay/Assets/Scripts/WaterOpendoor.cs
+++ GateWay/Assets/Scripts/WaterOpendoor.cs
@@ -5,55 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class WaterOpendoor : MonoBehaviour
 {
+    StarController starCon;
+
+    void Start()
+    {
+        starCon = GameObject.Find("Star").GetComponent<StarController>();
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Player")        // 너 플레이어랑 충돌했니??
+        if (starCon.star == true)
         {
-            if (SceneManager.GetActiveScene().name == "WaterStage1")      // 현재 스테이지 이름 확인하고
+            if (col.gameObject.tag == "Player")        // 너 플레이어랑 충돌했니??
             {
-                SceneManager.LoadScene("WaterStage2");                   // 맞으면 다음씬으로 넘어가
-            }
+                if (SceneManager.GetActiveScene().name == "WaterStage1")      // 현재 스테이지 이름 확인하고
+                {
+                    SceneManager.LoadScene("WaterStage2");                   // 맞으면 다음씬으로 넘어가
+                }
 
-            else if (SceneManager.GetActiveScene().name == "WaterStage2")
-            {
-                SceneManager.LoadScene("WaterStage3");
-            }
+                else if (SceneManager.GetActiveScene().name == "WaterStage2")
+                {
+                    SceneManager.LoadScene("WaterStage3");
+                }
 
-            else if (SceneManager.GetActiveScene().name == "WaterStage3")
-            {
-                SceneManager.LoadScene("WaterStage4");
-            }
+                else if (SceneManager.GetActiveScene().name == "WaterStage3")
+                {
+                    SceneManager.LoadScene("WaterStage4");
+                }
 
-            else if (SceneManager.GetActiveScene().name == "WaterStage4")
-            {
-                SceneManager.LoadScene("WaterStage5");
-            }
+                else if (SceneManager.GetActiveScene().name == "WaterStage4")
+                {
+                    SceneManager.LoadScene("WaterStage5");
+                }
 
-            else if (SceneManager.GetActiveScene().name == "WaterStage5")
-            {
-                SceneManager.LoadScene("WaterStage6");
-            }
+                else if (SceneManager.GetActiveScene().name == "WaterStage5")
+                {
+                    SceneManager.LoadScene("WaterStage6");
+                }
 
+            }
         }
-    }
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
 
     }
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
 
 

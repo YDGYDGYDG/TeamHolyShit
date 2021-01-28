e7df6a284ec46abd6af018cd9d050f137e939f93
KingChanwoo <77060802+KingChanwoo@users.noreply.github.com>
2021-01-28 12:52:24 +0900

    몬스터 스크립트 수정
    박스 충돌 시 몬스터 뒈짖(충돌된 박스 삭제)
    스테이지 1 수정 완료

--- GateWay/Assets/Asset/States/MonsterState.cs
+++ GateWay/Assets/Asset/States/MonsterState.cs
@@ -6,22 +6,25 @@ using UnityEngine.UI;
 public class MonsterState : MonoBehaviour
 {
 
-    HookShotScript hookLine;    // 훅 연결용 변수
+     HookShotScript hookLine;    // 훅 연결용 변수
+    
 
     public GameObject monsterDeath;
 
     GameObject box;             // 박스 연결
 
-    Rigidbody2D boxRigibody2D;  // 박스 물리 연결
+    //Rigidbody2D boxRigibody2D;  // 박스 물리 연결(임시)
 
     // Start is called before the first frame update
     void Start()
     {
 
         hookLine = GameObject.Find("player").GetComponent<HookShotScript>();    // 스크립트 연결
+       
+
         box = GameObject.FindGameObjectWithTag("Object");   // 박스 태그를 찾고
-        box.layer = LayerMask.NameToLayer("shootable");     // 레이어 이름 값을 찾아
-        boxRigibody2D = box.GetComponent<Rigidbody2D>();     // 박스 리지드 연결하고?
+        //box.layer = LayerMask.NameToLayer("shootable");     // 레이어 이름 값을 찾아(임시)
+        //boxRigibody2D = box.GetComponent<Rigidbody2D>();     // 박스 리지드 연결하고?(임시)
 
     }
 
@@ -33,15 +36,16 @@ public class MonsterState : MonoBehaviour
 
             this.gameObject.SetActive(false);   // 그럼 뒤지삼
             Instantiate(monsterDeath, transform.position, Quaternion.identity);  // 이펙트도 출력해
-            hookLine.HookOFF();                 // 훅도 지워줘야지??
+            hookLine.HookOFF() ;                 // 훅도 지워줘야지??
 
         }
 
-        else if (boxRigibody2D.velocity.y > 0 && col.gameObject)       
+        else if (col.gameObject.tag == "Object")       
         {
 
             this.gameObject.SetActive(false);   // 그럼 뒤지삼
             Instantiate(monsterDeath, transform.position, Quaternion.identity);  // 이펙트도 출력해
+            Destroy(box);       // 충돌한 박스는 없애
            
             
 

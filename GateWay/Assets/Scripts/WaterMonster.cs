c4be5d561fa5f069989fb83da1cc579b4926a661
Nayeon- <rlaskdus0102@gmail.com>
2021-01-29 23:50:24 +0900

    물 몬스터 제작

---
+++ GateWay/Assets/Scripts/WaterMonster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMonster : MonoBehaviour
+{
+    public int monsterSpeed = 100;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Water")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2Int(0, monsterSpeed));
+        
+    }
+}
---
+++ GateWay/Assets/Scripts/WaterMonsterGenerater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMonsterGenerater : MonoBehaviour
+{
+    GameObject player;
+    GameObject rangeLeft;
+    GameObject rangeRight;
+
+
+    public GameObject waterMonsterPrefab;
+    float span = 3f;
+    float delta = 0;
+
+
+
+    void Start()
+    {
+        player = GameObject.Find("player");
+        rangeLeft = GameObject.Find("rangeLeft");
+        rangeRight = GameObject.Find("rangeRight");
+    }
+
+    void Update()
+    {
+        if (player.transform.position.x > rangeLeft.transform.position.x && player.transform.position.x < rangeRight.transform.position.x)
+        {
+            delta += Time.deltaTime;
+            if (delta > span)
+            {
+                delta = 0;
+                GameObject waterMonster = Instantiate(waterMonsterPrefab) as GameObject;
+                waterMonster.transform.position = new Vector2(player.transform.position.x, rangeLeft.transform.position.y);
+            }
+        }
+    }
+}

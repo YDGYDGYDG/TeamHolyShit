99e062abac1ba8ec2395a65d2ef89eb77e61270d
hjuni0524 <hjuni0524@naver.com>
2021-01-29 12:09:55 +0900

    'OpenDoor' 스크립트 모든 씬 연결

--- GateWay/Assets/Asset/States/OpenDoor.cs
+++ GateWay/Assets/Asset/States/OpenDoor.cs
@@ -9,33 +9,99 @@ public class OpenDoor : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
     {
         
-       if (col.gameObject.tag == "Player")        // 너 플레이어랑 충돌했니??
+       if (col.gameObject.tag == "Player")  // 너 플레이어랑 충돌했니??
        {
-            if (SceneManager.GetActiveScene().name == "LavaStage1")      // 현재 스테이지 이름 확인하고
+            // =========================== 김형준(용암) =====================================
+            if (SceneManager.GetActiveScene().name == "LavaStage1")    // 현재 스테이지 이름 확인하고
             {
-                SceneManager.LoadScene("LavaStage2");                   // 맞으면 다음씬으로 넘어가
+                SceneManager.LoadScene("LavaStage2");    // 맞으면 다음씬으로 넘어가(2 스테이지 이동)
             }
 
             else if (SceneManager.GetActiveScene().name == "LavaStage2")
             {
-                SceneManager.LoadScene("LavaStage3");
+                SceneManager.LoadScene("LavaStage3");   // 3 스테이지 이동
             }
 
             else if (SceneManager.GetActiveScene().name == "LavaStage3")
             {
-                SceneManager.LoadScene("LavaStage4");
+                SceneManager.LoadScene("LavaStage4");   // 4 스테이지 이동
             }
 
             else if (SceneManager.GetActiveScene().name == "LavaStage4")
             {
-                SceneManager.LoadScene("LavaStage5");
+                SceneManager.LoadScene("LavaStage5");   // 5 스테이지 이동
             }
 
             else if (SceneManager.GetActiveScene().name == "LavaStage5")
             {
-                SceneManager.LoadScene("LavaStage6");
+                SceneManager.LoadScene("LavaStage6");   // 6 스테이지 이동
+            }
+            //=============================================================================
+
+
+
+
+            // =========================== 김나연(물) =====================================
+            else if (SceneManager.GetActiveScene().name == "WaterStage1")
+            {
+                SceneManager.LoadScene("WaterStage2");  // 2 스테이지 이동
+            }
+            //=============================================================================
+
+
+
+
+            // =========================== 김휘원(튜토리얼) =====================================
+            else if (SceneManager.GetActiveScene().name == "TutorialStage1")
+            {
+                SceneManager.LoadScene("TutorialStage2");   // 2 스테이지 이동
             }
 
+            else if (SceneManager.GetActiveScene().name == "TutorialStage2")
+            {
+                SceneManager.LoadScene("TutorialStage3");   // 3 스테이지 이동
+            }
+            //=============================================================================
+
+
+
+
+            // =========================== 유건희(바람) =====================================
+            else if (SceneManager.GetActiveScene().name == "InfinityStorm4-1")
+            {
+                SceneManager.LoadScene("InfinityStorm4-2"); // 2 스테이지 이동
+            }
+
+            else if (SceneManager.GetActiveScene().name == "InfinityStorm4-2")
+            {
+                SceneManager.LoadScene("InfinityStorm4-3"); // 3 스테이지 이동
+            }
+
+            else if (SceneManager.GetActiveScene().name == "InfinityStorm4-3")
+            {
+                SceneManager.LoadScene("InfinityStorm4-4"); // 4 스테이지 이동
+            }
+
+            else if (SceneManager.GetActiveScene().name == "InfinityStorm4-4")
+            {
+                SceneManager.LoadScene("InfinityStorm4-5"); // 5 스테이지 이동
+            }
+            //=============================================================================
+
+
+
+
+            // =========================== 윤도균(기계) =====================================
+            else if (SceneManager.GetActiveScene().name == "MachineryStage0")
+            {
+                SceneManager.LoadScene("MachineryStage1"); // 1 스테이지 이동
+            }
+
+            else if (SceneManager.GetActiveScene().name == "MachineryStage1")
+            {
+                SceneManager.LoadScene("MachineryStage2"); // 2 스테이지 이동
+            }
+            //=============================================================================
         }
     }
 

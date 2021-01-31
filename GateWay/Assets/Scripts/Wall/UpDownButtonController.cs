bad8759f89072ed95a523bd073a1b5ee45d1c0e7
rlagnldnjs <jaj424444@gmail.com>
2021-01-31 22:01:43 +0900

    버튼 벽 스크립트 수정

--- GateWay/Assets/Scripts/Wall/UpDownButtonController.cs
+++ GateWay/Assets/Scripts/Wall/UpDownButtonController.cs
@@ -5,40 +5,69 @@ using UnityEngine;
 public class UpDownButtonController : MonoBehaviour
 {
     public bool buttonOn;
+
+    bool playerOn;
+    bool objectOn;
+
+    float speed = 0.01f;
+    float maxSpeed = 0.03f;
+
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+    }
+    private void Awake()
+    {
+
+        startPos = gameObject.transform.position;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            buttonOn = true;
-            gameObject.transform.Translate(0, -0.05f, 0);
+            playerOn = true;
         }
         else if (collision.transform.tag == "Object")
         {
-            buttonOn = true;
-            gameObject.transform.Translate(0, -0.05f, 0);
+            objectOn = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            buttonOn = false;
-            gameObject.transform.Translate(0, 0.05f, 0);
+            playerOn = false;
         }
         else if (collision.transform.tag == "Object")
         {
-            buttonOn = false;
-            gameObject.transform.Translate(0, 0.05f, 0);
+            objectOn = false;
         }
     }
+    void ButtonOn()
+    {
+        buttonOn = true;
+        if ( transform.position.y > startPos.y - maxSpeed)
+        gameObject.transform.Translate(0, -speed, 0);
+    }
+    void ButtonOff()
+    {
+        buttonOn = false;
+        if ( transform.position.y < startPos.y + maxSpeed)
+        gameObject.transform.Translate(0, speed, 0);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (playerOn || objectOn)
+        {
 
+            ButtonOn();
+        }
+        else if (!playerOn && !objectOn)
+        {
+            ButtonOff();
+        }
     }
 }

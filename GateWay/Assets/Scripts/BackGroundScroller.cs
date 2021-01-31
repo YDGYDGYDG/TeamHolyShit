93052d4e405f5feadc930134e212c13a72111471
YDGYDGYDG <tlsaoss@naver.com>
2021-01-31 21:18:47 +0900

    배경 스크롤러

---
+++ GateWay/Assets/Scripts/BackGroundScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroller : MonoBehaviour
+{
+    public Transform camera;
+
+    MeshRenderer renderer;
+
+    public float speed = 0.03f;
+
+    float offsetX;
+    float offsetY;
+
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        offsetX = camera.position.x;
+        offsetY = camera.position.y;
+    }
+
+    void Update()
+    {
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX * speed, offsetY * speed));
+        offsetX = camera.position.x;
+        offsetY = camera.position.y;
+
+    }
+}

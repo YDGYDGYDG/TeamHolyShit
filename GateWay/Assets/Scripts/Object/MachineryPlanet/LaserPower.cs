25b331c4262ea85f27a6f08b1342531bbe9d8ca4
YDGYDGYDG <tlsaoss@naver.com>
2021-02-02 21:41:01 +0900

    기계행성 리소스
    레이저 전원

---
+++ GateWay/Assets/Scripts/Object/MachineryPlanet/LaserPower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPower : MonoBehaviour
+{
+    public GameObject powerDownObject;
+    public GameObject powerVisual;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Shootable"))
+        {
+            powerVisual.SetActive(false);
+            powerDownObject.SetActive(false);
+        }
+    }
+}

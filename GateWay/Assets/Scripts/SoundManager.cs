f26438c71df2deccfb165024e4f9463a771a9dcd
YDGYDGYDG <tlsaoss@naver.com>
2021-02-03 21:53:05 +0900

    기계행성 사운드

---
+++ GateWay/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // 오디오 클립
+    public AudioClip [] audioClips;
+    /*
+     * 0: 
+     * 1: 
+     * 2:
+     * 3: 
+     * 4: 
+     * 5: 
+     * 6: 
+     * 7: 
+     * 8: 
+     * 9: 
+     * 10: 
+     */
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+
+    }
+
+    public void ActiveSoundOneShot(int index)
+    {
+        audioSource.PlayOneShot(audioClips[index]);
+    }
+
+    public void ActiveSoundBGM(int index)
+    {
+        audioSource.clip = audioClips[index];
+        audioSource.Play();
+    }
+
+}

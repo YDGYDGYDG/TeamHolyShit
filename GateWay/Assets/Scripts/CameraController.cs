2633bea446e5773ad1917e444322cb1bbcbf205a
YDGYDGYDG <tlsaoss@naver.com>
2021-02-04 11:43:50 +0900

    카메라 트래킹 수정
    카메라가 정말 제대로 화면을 제한하고 따라감

--- GateWay/Assets/Scripts/CameraController.cs
+++ GateWay/Assets/Scripts/CameraController.cs
@@ -17,9 +17,80 @@ public class CameraController : MonoBehaviour
     void FixedUpdate()
     {
         smoothedPosition = Vector3.Lerp(playerTransform.position, transform.position, smoothSpeed);
+
         if (transform.position.x < widthMax && transform.position.x > widthMin && transform.position.y < heightMax && transform.position.y > heightMin)
         {
             transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+        }
+        // 전부 이탈
+        // XY 초과
+        else if (transform.position.x >= widthMax && transform.position.y >= heightMax)
+        {
+            transform.position = new Vector3(widthMax, heightMax, transform.position.z);
+            if (playerTransform.position.x <= widthMax)
+                transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+            if (playerTransform.position.y <= heightMax)
+                transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+        }
+        // XY 미만
+        else if (transform.position.x <= widthMin && transform.position.y <= heightMin)
+        {
+            transform.position = new Vector3(widthMin, heightMin, transform.position.z);
+            if (playerTransform.position.x >= widthMin)
+                transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+            if (playerTransform.position.y >= heightMin)
+                transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+
+        }
+        // X초과 Y미만
+        else if (transform.position.x >= widthMax && transform.position.y <= heightMin)
+        {
+            transform.position = new Vector3(widthMax, heightMin, transform.position.z);
+            if (playerTransform.position.x <= widthMax)
+                transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+            if (playerTransform.position.y >= heightMin)
+                transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+
+        }
+        // X미만 Y초과
+        else if (transform.position.x <= widthMin && transform.position.y >= heightMax)
+        {
+            transform.position = new Vector3(widthMin, heightMax, transform.position.z);
+            if (playerTransform.position.x >= widthMin)
+                transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
+            if (playerTransform.position.y <= heightMax)
+                transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+
+        }
+        // X 이탈
+        else if (transform.position.x >= widthMax)
+        {
+            transform.position = new Vector3(widthMax, smoothedPosition.y, transform.position.z);
+            if (playerTransform.position.x <= widthMax)
+                transform.position = new Vector3(playerTransform.position.x, smoothedPosition.y, transform.position.z);
+        }
+        else if (transform.position.x <= widthMin)
+        {
+            transform.position = new Vector3(widthMin, smoothedPosition.y, transform.position.z);
+            if (playerTransform.position.x >= widthMin)
+                transform.position = new Vector3(playerTransform.position.x, smoothedPosition.y, transform.position.z);
+        }
+        // Y 이탈
+        else if (transform.position.y >= heightMax)
+        {
+            transform.position = new Vector3(smoothedPosition.x, heightMax, transform.position.z);
+            if (playerTransform.position.y <= heightMax)
+                transform.position = new Vector3(smoothedPosition.x, playerTransform.position.y, transform.position.z);
+        }
+        else if (transform.position.y <= heightMin)
+        {
+            transform.position = new Vector3(smoothedPosition.x, heightMin, transform.position.z);
+            if (playerTransform.position.y >= heightMin)
+                transform.position = new Vector3(smoothedPosition.x, playerTransform.position.y, transform.position.z);
+        }
+        else
+        {
+            Debug.Log("카메라 오류);
         }
     }
 }

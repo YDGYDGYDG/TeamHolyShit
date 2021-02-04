c32040e95d6ece32af296c454a2fd1ba288177fa
YooKeonHee <rjsgml4353@naver.com>
2021-02-04 23:24:46 +0900

    스타트화면 중간작업
    스타트화면 중간작업

---
+++ GateWay/Assets/Scripts/MainIntro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainIntro : MonoBehaviour
+{
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time < 0.5f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - time);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time);
+            if(time > 1f)
+            {
+                time = 0;
+            }
+        }
+        time += Time.deltaTime;
+    }
+}

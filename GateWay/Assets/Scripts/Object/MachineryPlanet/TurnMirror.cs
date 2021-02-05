513e2b04a4663fa6c004befbc17e92e2ec3d8c36
YDGYDGYDG <tlsaoss@naver.com>
2021-02-05 15:20:52 +0900

    회전 거울 프리팹

---
+++ GateWay/Assets/Scripts/Object/MachineryPlanet/TurnMirror.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMirror : MonoBehaviour
+{
+    int count = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Hook") 
+        {
+            count += 1;
+            transform.rotation = Quaternion.Euler(0, 0, 60 * count);
+            Debug.Log(count);
+            if (count >= 6) count = 0;
+        }
+    }
+}

309e3e7ffcdb504cf0e8ea2ea590b79193632020
hjuni0524 <hjuni0524@naver.com>
2021-02-06 13:35:01 +0900

    GreyGenerator 주석 처리 -> 사유 : 에러때문에 작업 불가능

--- GateWay/Assets/Scripts/Boss/GreyGenerator.cs
+++ GateWay/Assets/Scripts/Boss/GreyGenerator.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class GreyGenerator : MonoBehaviour
 {
-    float time;  // 경과 시간
-    float WallReset; // 벽 초기화
-    public float ATime = 1.0f;
-    public float BTime = 1.0f;
-    public GameObject rlr;
-    public GameObject rlb;
-    public GameObject udr;
-    public GameObject udb;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
-    }
-
-    void AWalls()
-    {
-        Bwall1.GetComponent<SpriteRenderer>().material.color = new Color(255, 255, 255, 0);
-        Bwall1.GetComponent<BoxCollider2D>().isTrigger = true;
-        Bwall1.gameObject.tag = "Untagged";
-    }
-    void BWalls()
-    {
-        Bwall1.GetComponent<SpriteRenderer>().material.color = Color.white;
-        Bwall1.GetComponent<BoxCollider2D>().isTrigger = false;
-        Bwall1.gameObject.tag = "Wall";
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        WallReset = BTime + ATime;
-
-        time += Time.deltaTime;
-        if (time < ATime)
-            AWalls();
-        if (time >= BTime)
-            BWalls();
-
-        if (time > WallReset)
-            time = 0;
-    }
+  //  float time;  // 경과 시간
+  //  float WallReset; // 벽 초기화
+  //  public float ATime = 1.0f;
+  //  public float BTime = 1.0f;
+  //  public GameObject rlr;
+  //  public GameObject rlb;
+  //  public GameObject udr;
+  //  public GameObject udb;
+  //
+  //
+  //  // Start is called before the first frame update
+  //  void Start()
+  //  {
+  //
+  //
+  //  }
+  //
+  //  void AWalls()
+  //  {
+  //      Bwall1.GetComponent<SpriteRenderer>().material.color = new Color(255, 255, 255, 0);
+  //      Bwall1.GetComponent<BoxCollider2D>().isTrigger = true;
+  //      Bwall1.gameObject.tag = "Untagged";
+  //  }
+  //  void BWalls()
+  //  {
+  //      Bwall1.GetComponent<SpriteRenderer>().material.color = Color.white;
+  //      Bwall1.GetComponent<BoxCollider2D>().isTrigger = false;
+  //      Bwall1.gameObject.tag = "Wall";
+  //  }
+  //  // Update is called once per frame
+  //  void Update()
+  //  {
+  //      WallReset = BTime + ATime;
+  //
+  //      time += Time.deltaTime;
+  //      if (time < ATime)
+  //          AWalls();
+  //      if (time >= BTime)
+  //          BWalls();
+  //
+  //      if (time > WallReset)
+  //          time = 0;
+  //  }
 }

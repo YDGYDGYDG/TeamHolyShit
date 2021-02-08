918cfd8ac298bbe94b02eb59ec2ab75fada0bae0
YDGYDGYDG <tlsaoss@naver.com>
2021-02-08 15:23:10 +0900

    기계맵 포탈 사운드

--- GateWay/Assets/Scripts/Object/MachineryPlanet/Portal.cs
+++ GateWay/Assets/Scripts/Object/MachineryPlanet/Portal.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public GameObject portalOut;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.transform.position = portalOut.transform.position + portalOut.transform.up;
+            audioSource.Play();
         }
     }
 }

edbb969d836fb8728f1cf2fba5f8828684ce5459
rlagnldnjs <77229894+lms5330@users.noreply.github.com>
2021-02-09 09:52:55 +0900

    게임 화면 고정 / 점프 버튼 수정

---
+++ GateWay/Assets/Scripts/Boss/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        // Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(720, 1280, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

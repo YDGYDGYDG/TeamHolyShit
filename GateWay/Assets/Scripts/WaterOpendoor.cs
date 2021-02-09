f5f80b8d1b3536f4084abd99498917221cf78f74
Patapon <samanny90@naver.com>
2021-02-09 09:36:48 +0900

    -물-
    물이 물했다 ^^

--- GateWay/Assets/Scripts/WaterOpendoor.cs
+++ GateWay/Assets/Scripts/WaterOpendoor.cs
@@ -22,21 +22,25 @@ public class WaterOpendoor : MonoBehaviour
                 if (SceneManager.GetActiveScene().name == "WaterPlanet1")      // 현재 스테이지 이름 확인하고
                 {
                     SceneManager.LoadScene("WaterPlanet2");                   // 맞으면 다음씬으로 넘어가
+                    PlayerPrefs.SetInt("Water", 1);
                 }
 
                 else if (SceneManager.GetActiveScene().name == "WaterPlanet2")
                 {
                     SceneManager.LoadScene("WaterPlanet3");
+                    PlayerPrefs.SetInt("Water", 2);
                 }
 
                 else if (SceneManager.GetActiveScene().name == "WaterPlanet3")
                 {
                     SceneManager.LoadScene("WaterPlanet4");
+                    PlayerPrefs.SetInt("Water", 3);
                 }
 
                 else if (SceneManager.GetActiveScene().name == "WaterPlanet4")
                 {
                     SceneManager.LoadScene("ClearScene"); // 클리어 화면으로 이동
+                    PlayerPrefs.SetInt("Water", 4);
                 }
 
             }
